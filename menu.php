5b1bc85332439dbc0f577a02eaf753978542dc6d
winston <andyredjohn@gmail.com>
2025-02-05 19:23:42 -0600

    update register.php

--- menu.php
+++ menu.php
@@ -13,6 +13,7 @@ function isLoggedIn()
   <li><a href="index.php" <?= ($g_page == 'index') ? "class='active'" : '' ?>>Home</a></li>
   <li><a href="create.php" <?= ($g_page == 'create') ? "class='active'" : '' ?>>New Post</a></li>
   <li><a href="login.php" <?= ($g_page == 'login') ? "class='active'" : '' ?>>Login</a></li>
+  <li><a href="register.php" <?= ($g_page == 'register') ? "class='active'" : '' ?>>Register</a></li>
 
   <li><a href="Privacy.php" <?= ($g_page == 'privacy') ? "class='active'" : '' ?>>Privacy</a></li>
 </ul> <!-- END div id="menu" -->
--- register.php
+++ register.php
@@ -1,14 +1,24 @@
+<?php
+require 'config.php';
+require 'database.php';
+$g_title = BLOG_NAME . ' - Index';
+$g_page = 'login';
+require 'header.php';
+require 'menu.php';
+
+?>
+<div id="all_blogs">
 <html>
 	<head>
 		<title>Main Register Page</title>
 	</head>
 	<body>
 
-		<table width="300" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
+		<table width="300" border="0"  cellpadding="0" cellspacing="1" >
 			<tr>
 				<form name="form1" method="post" action="process_register.php">
 					<td>
-						<table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
+						<table width="100%" border="0" cellpadding="3" cellspacing="1" >
 							<tr>
 								<td colspan="3"><strong>Member Register </strong></td>
 							</tr>
@@ -18,9 +28,19 @@
 								<td width="294"><input name="myusername" type="text" id="myusername"></td>
 							</tr>
 							<tr>
+								<td width="78">E-mail</td>
+								<td width="6">:</td>
+								<td width="294"><input name="myemail" type="email" id="myemail"></td>
+							</tr>
+							<tr>
 								<td>Password</td>
 								<td>:</td>
-								<td><input name="mypassword" type="text" id="mypassword"></td>
+								<td><input name="mypassword" type="password" id="mypassword"></td>
+							</tr>
+							<tr>
+								<td>Verify Password</td>
+								<td>:</td>
+								<td><input name="verify_password" type="password" id="verify_password"></td>
 							</tr>
 							<tr>
 								<td>&nbsp;</td>
@@ -36,3 +56,7 @@
 
 	</body>
 </html>
+</div>
+<?php
+require 'footer.php';
+?>
